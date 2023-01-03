ee88a8b0b188242ff5011fd192ada2d0220e49c7
technik-matrose <121881667+technik-matrose@users.noreply.github.com>
2023-01-03 17:01:27 +0100

    Update hello.py
    Add an undeclared variable for lint testing

--- hello.py
+++ hello.py
@@ -3,6 +3,7 @@ import click
 @click.command(help="This is just a hello app. It does nothing.")
 @click.option("--name", prompt="I need your name", help="Need name")
 @click.option("--color", prompt="I need your color", help="This is your color")
+var=
 def hello(name, color):
     if name == "Thor":
         click.echo("Thor, you are always red.")
